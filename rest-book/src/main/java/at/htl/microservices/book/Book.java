8dd4758d958dc9f5f14fd19ca6d580913c104fc8
sKnogler <simon.knogler@liwest.at>
2021-10-11 14:04:53 +0200

    Book.java added

---
+++ rest-book/src/main/java/at/htl/microservices/book/Book.java
@@ -0,0 +1,14 @@
+package at.htl.microservices.book;
+
+import java.time.Instant;
+
+public class Book {
+    private String isbn13;
+    private String title;
+    private String author;
+    private String genre;
+    private Instant instant_createTime;
+    private int yearOfPublication;
+
+
+}
